7f838f756e0e84647b6127602a5f9c130867d8bf
guschins <guschins@gmail.com>
2022-12-22 10:14:33 +0300

    fix: remove bug render category

--- online-store/src/components/loader.js
+++ online-store/src/components/loader.js
@@ -16,30 +16,28 @@ if (app) {
 }
 
 function renderProducts(array) {
-    console.log('array: ', array);
     let fragment = new DocumentFragment();
     let brands = [];
     let category = [];
     array.forEach((data, index) => {
         let card = new CatalogCard(data, index);
-        console.log('card: ', card);
         brands.push(data.brand);
         category.push(data.category);
         fragment.append(card.renderCard());
+    });
+    category = [...new Set(category)];
+    const filtersCategory = document.querySelector('.filters-category-list');
+    category.forEach((item) => {
+        let category = new CategoriesBrands(item);
+        filtersCategory.append(category.renderCategoriesBrands());
     });
     brands = [...new Set(brands)];
     const filtersBrand = document.querySelector('.filters-brand-list');
     brands.forEach((item) => {
         let brand = new CategoriesBrands(item);
-        console.log(item);
         filtersBrand.append(brand.renderCategoriesBrands());
     });
-    category = [...new Set(category)];
-    const filtersCategory = document.querySelector('.filters-category-list');
-    brands.forEach((item) => {
-        let category = new CategoriesBrands(item);
-        filtersCategory.append(category.renderCategoriesBrands());
-    })
+
     const cardsList = document.querySelector('.cards__list');
     cardsList.append(fragment);
     return cardsList;
