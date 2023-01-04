5b6e33c32bb212b5dabdafecbcde4feb41da6234
guschins <guschins@gmail.com>
2023-01-04 14:54:50 +0300

    fix: remove unnecessary

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,13 +1,3 @@
-// import App from './components/app/app';
 import './scss/main.scss';
-import './components/header';
-import './components/main';
 import './components/render';
-// import './components/modal';
-// import './components/overlay';
-import './components/product-catalog';
-import './components/catalog-card';
 import './components/loader';
-
-// const app = new App();
-// app.start();
